248b74f31afaccc0a8c74caf503f679cfee2c1d4
Alexis Dutaud <alexis@MacBook-Pro-de-Alexis.local>
2025-02-24 11:00:35 +0100

    wip: integration logo

--- app/src/main/java/fr/isen/ISENSmartCompanion/isensmartcompanion/MainActivity.kt
+++ app/src/main/java/fr/isen/ISENSmartCompanion/isensmartcompanion/MainActivity.kt
@@ -1,15 +1,19 @@
 package fr.isen.ISENSmartCompanion.isensmartcompanion
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import fr.isen.ISENSmartCompanion.isensmartcompanion.ui.theme.ISENSmartCompanionTheme
 
@@ -21,7 +25,7 @@ class MainActivity : ComponentActivity() {
             ISENSmartCompanionTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = getString(R.string.app_name),
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -32,10 +36,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column {
+        Image(
+            painterResource(R.drawable.isen_logo),
+            "Isen Logo Header"
+        )
+        Text(
+            text = "$name!",
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
